54c581ad55f2b862034b3aa71fb04268467bf2ce
WeeDolf <WeeDolf@192.168.0.21>
2016-05-19 00:59:06 +0200

    prueba1

---
+++ Desktop/Boletin12-master/src/boletin12/Boletin12.java
@@ -0,0 +1,27 @@
+package boletin12;
+
+public class Boletin12 {
+
+    public static void main(String[] args) {
+        
+        
+        Garaje par = new Garaje();
+        Garaje par2 = new Garaje();
+        Garaje par3 = new Garaje();
+        Garaje par4 = new Garaje();
+        Garaje par5 = new Garaje();
+        Garaje par6 = new Garaje();
+
+        par.entraCoche();
+        par.prueba(); 
+        par2.entraCoche();
+        par3.entraCoche();
+        par4.entraCoche();
+        par5.entraCoche();
+        par6.entraCoche();
+        par.factura();
+        par2.factura();
+        par3.factura();
+
+    }
+}
